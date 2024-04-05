79b89967618aa685dbaa8a2ad99f8fdd6faf558b
Shinichi Yanagido <hakuto1yds@gmail.com>
2024-04-06 00:13:19 +0900

    add formula

---
+++ cf-ps1.rb
@@ -0,0 +1,10 @@
+class CfPs1 < Formula
+  desc "Cloud Foundry prompt for bash and zsh"
+  homepage "https://github.com/mdan16/cf-ps1"
+  head "https://github.com/mdan16/cf-ps1.git", branch: "main"
+  license "MIT"
+
+  def install
+    bin.install "cf-ps1.sh"
+  end
+end
